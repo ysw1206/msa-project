a8e73121c3b2a03a6975d5820919e5a863bbf8bd
Sun ung yang <ysw1206@gmail.com>
2025-06-14 23:14:36 +0900

    auth-service: add security dependencies and user details service

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -8,6 +8,9 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.37")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation(project(":common-lib"))
